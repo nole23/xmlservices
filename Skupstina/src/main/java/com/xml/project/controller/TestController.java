5327e23a3d531ec0d3ab93f719003a0a970e9a88
Novica Nikolic <nole0223@gmail.com>
2016-12-30 19:13:31 +0100

    Cuvanje u bazu kao i brisanje iz baze

--- Skupstina/src/main/java/com/xml/project/controller/TestController.java
+++ Skupstina/src/main/java/com/xml/project/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.xml.project.controller;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.DatabaseClientFactory;
+import com.marklogic.client.document.XMLDocumentManager;
+import com.marklogic.client.io.InputStreamHandle;
 import com.xml.project.util.DatabaseUtil;
 
 @RestController
@@ -16,12 +22,58 @@ public class TestController {
 	private DatabaseClient databaseClient;
 	private DatabaseUtil dUtil = new DatabaseUtil();
 
-	@RequestMapping(value = "/get", method = RequestMethod.GET)
-	public ResponseEntity<String> saveAdmin() {
-		// databaseClient = DatabaseClientFactory.newClient(dUtil.getHost(),
-		// dUtil.getPort(), dUtil.getUsername(), dUtil.getPassword(),
-		// dUtil.getAuthType());
+	/**
+	 * Cuvanje seme u MarkLogic bazu
+	 * 
+	 * @return
+	 * @throws FileNotFoundException
+	 */
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public ResponseEntity<String> saveAdmin() throws FileNotFoundException {
+		
+		
+		 databaseClient = DatabaseClientFactory.newClient(dUtil.getHost(),
+		 dUtil.getPort(), dUtil.getDatabase(), dUtil.getUsername(), dUtil.getPassword(),
+		 dUtil.getAuthType());
+		 
+		 XMLDocumentManager xmlMenager = databaseClient.newXMLDocumentManager();
+		 
+		 //Pod kojim imenom ce sacuvati u Bazu podatak
+		 String docId = "/example/books.xml";
+		 
+		 //XML koji zelimo da sacuvamo u bazu
+		 InputStreamHandle handle = new InputStreamHandle(new FileInputStream("data/books.xml"));
+		 
+		 //Pisanje u bazu
+		 xmlMenager.write(docId, handle);
+		 
+		 databaseClient.release();
 
-		return new ResponseEntity<String>(dUtil.getAuth_type() != null ? dUtil.getAuth_type() : "Null", HttpStatus.OK);
+		return new ResponseEntity<String>("Dodato", HttpStatus.OK);
 	}
+	
+	/**
+	 * Brisanje iz MarkLogic baze
+	 * @return
+	 * @throws FileNotFoundException
+	 */
+	@RequestMapping(value = "/delete", method = RequestMethod.GET)
+	public ResponseEntity<String> deleteXML() throws FileNotFoundException {
+		 databaseClient = DatabaseClientFactory.newClient(dUtil.getHost(),
+		 dUtil.getPort(), dUtil.getDatabase(), dUtil.getUsername(), dUtil.getPassword(),
+		 dUtil.getAuthType());
+		 
+		 XMLDocumentManager xmlMenager = databaseClient.newXMLDocumentManager();
+		 
+		 //Dokument u bazi koji zelimo da izbrisemo
+		 String docId = "/example/books.xml";
+		 
+		 //brisanje iz baze dati dokument
+		 xmlMenager.delete(docId);
+		 
+		 databaseClient.release();
+
+		return new ResponseEntity<String>("Obrisano", HttpStatus.OK);
+	}
+	
 }
