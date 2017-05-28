9edf19341f1b2e0aa15c52b2618cbd946e6d2aa6
Novica Nikolic <nole0223@gmail.com>
2017-05-28 20:48:36 +0200

    accept act

--- Skupstina/src/main/java/com/xml/project/controller/ActController.java
+++ Skupstina/src/main/java/com/xml/project/controller/ActController.java
@@ -2,10 +2,8 @@ package com.xml.project.controller;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.security.Principal;
 
 import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
@@ -31,11 +29,9 @@ import com.marklogic.client.document.DocumentDescriptor;
 import com.marklogic.client.document.DocumentUriTemplate;
 import com.marklogic.client.document.XMLDocumentManager;
 import com.marklogic.client.io.DocumentMetadataHandle;
+import com.marklogic.client.io.DocumentMetadataHandle.DocumentCollections;
 import com.marklogic.client.io.InputStreamHandle;
-import com.marklogic.client.io.JAXBHandle;
-import com.xml.project.jaxb.Clan;
 import com.xml.project.jaxb.Dokument;
-import com.xml.project.model.User;
 import com.xml.project.service.UserService;
 import com.xml.project.util.DatabaseUtil;
 
@@ -99,6 +95,30 @@ public class ActController {
 		databaseClient.release();
 		
 		return new ResponseEntity<String>("Dokument je snimljen u bazu.", HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/accept/{docId}", method = RequestMethod.GET)
+	public ResponseEntity<String> acceptAct(@PathVariable String docId)
+			throws JAXBException, IOException, SAXException {
+		
+		//connecti to marklogic
+		databaseClient = DatabaseClientFactory.newClient(dUtil.getHost(), dUtil.getPort(), dUtil.getDatabase(),
+				dUtil.getUsername(), dUtil.getPassword(), dUtil.getAuthType());
+		XMLDocumentManager xmlMenager = databaseClient.newXMLDocumentManager();
+
+		xmlMenager = databaseClient.newXMLDocumentManager();
+		
+		String doc = "/acts/decisions/"+docId+".xml";
+		DocumentMetadataHandle metadata = new DocumentMetadataHandle();
+		xmlMenager.readMetadata(doc, metadata);
+		
+		DocumentCollections collections = metadata.getCollections();
+		collections.remove("/parliament/acts/proposed");
+		collections.add("/parliament/acts/accepted");
+		xmlMenager.writeMetadata(doc, metadata);
+		
+		databaseClient.release();
+		return new ResponseEntity<String>("Dokument je prihvacen.", HttpStatus.OK);
 	}
 	
 	/*
